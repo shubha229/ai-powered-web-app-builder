2bbfb3d34f27a92ea9a0071dee67856b0e4bd62d
shubha229 <shubhashreenayak9825@gmail.com>
2026-04-26 19:44:18 +0530

    Add production backend url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://ai-powered-web-app-builder-4ocn.onrender.com/api';
 
 const getHeaders = () => {
   const token = Cookies.get('token');
